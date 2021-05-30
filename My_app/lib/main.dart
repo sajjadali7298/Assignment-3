5148f944a0588b016f0fef208c90814e1047b624
sajjad ali <sajjadjaan803@gmail.com>
2021-05-30 10:49:37 +0500

    Work Accomplish

---
+++ My_app/lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.teal,
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/3.jpg'), fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.all(12.0),
+                  padding: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, 5),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.email,
+                          color: Color.fromRGBO(143, 148, 251, 1),
+                        ),
+                        hintText: 'Enter Your Email',
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                      onSaved: (String value) {},
+                      validator: (String value) {
+                        return value.contains('@') ? 'do not use @ char' : null;
+                      }),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.all(12.0),
+                  padding: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10))
+                      ]),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.lock,
+                          color: Color.fromRGBO(143, 148, 251, 1),
+                        ),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                      onSaved: (String value) {},
+                      validator: (String value) {
+                        return value.contains('@') ? 'do not use @ char' : null;
+                      }),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  padding: EdgeInsets.all(8.0),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.teal,
+                    // gradient: LinearGradient(colors: [
+                    //   // Color.fromRGBO(143, 148, 251, 1),
+                    //   // Color.fromRGBO(143, 148, 251, 6),
+                    // ])
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 70.0,
+                ),
+                Text(
+                  'Forget Password?',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
